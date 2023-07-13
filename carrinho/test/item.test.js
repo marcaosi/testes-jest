923c3a3697afe21612e2d803b04527fa5483ef93
Marcos Antonio Dos Santos <marcosantonio@fuvs.br>
2023-07-13 13:18:09 -0300

    aula

---
+++ carrinho/test/item.test.js
@@ -0,0 +1,16 @@
+import Item from '../item';
+
+describe('Testes dos itens', () => {
+    it('Deve ter 3 campos: campos, valor e quantidade', () => {
+        const item = new Item('Beterraba', 2.5, 10);
+
+        expect(item.nome).toBe('Beterraba');
+        expect(item.valor).toBe(2.5);
+        expect(item.quantidade).toBe(10);
+    });
+
+    it('Deve ter o preço calculado de acordo com a quantidade', () => {
+        const item = new Item('Batata', 0.3, 1);
+        expect(item.pegaValorTotalItem()).toBeCloseTo(0.3)
+    });
+});
